41178d07d3042c5a0b563ac563ff377be98159ca
re11n <97107417+re11n@users.noreply.github.com>
2023-04-26 16:47:31 +0700

    menambahkan API Movie

--- modul9_1302200003/Movie.cs
+++ modul9_1302200003/Movie.cs
@@ -2,5 +2,20 @@
 {
     public class Movie
     {
+
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public List<string> Stars { get; set; }
+        public String Description { get; set; }
+        public Movie(string v1, string v2, List<string> stars2, string v3)
+        {
+            this.Title = v1;
+            this.Director = v2;
+            this.Stars = stars2;
+            this.Description = v3;
+        }
+
+
+
     }
 }
--- modul9_1302200003/Program.cs
+++ modul9_1302200003/Program.cs
@@ -1,8 +1,8 @@
+using modul9_1302200003;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -18,8 +18,51 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+var movies = new List<Movie>()
+{
+    new Movie("The Shawshank Redemption", "Frank Darabont",
+            new List<string> { "Tim Robbins", "Morgan Freeman", "Bob Gunton", "William Sadler" },
+            "Two imprisoned men bond over a number of years, finding solace and eventual redemption " +
+            "through acts of common decency."
+            ),
+    new Movie("The Godfather", "Francis Ford Coppola",
+            new List<string> { " Marlon Brando", "Al Pacino", "James Caan", "Diane Keaton" },
+            "The aging patriarch of an organized crime dynasty in postwar New York City transfers " +
+            "control of his clandestine empire to his reluctant youngest son."
+            ),
+    new Movie("The Dark Knight", "Christopher Nolan",
+            new List<string> { "Christian Bale", "Heath Ledger", "Aaron Eckhart", "Michael Caine" },
+            "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, " +
+            "Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice."
+            )
 
-app.MapControllers();
+};
+
+app.MapGet("/api/Movies", () =>
+{
+    return movies;
+})
+    .WithName("GetMovies");
+
+app.MapPost("/api/Movies", async (Movie newMovie) =>
+{
+    movies.Add(newMovie);
+})
+    .WithName("PostMovie");
+
+app.MapGet("/api/Movies/{id}", async (int id) =>
+{
+    return movies[id];
+})
+    .WithName("GetMovie");
+
+app.MapDelete("/api/Movies/{id}", async (int id) =>
+{
+    movies.RemoveAt(id);
+})
+    .WithName("DeleteMovie");
+
+
+
 
 app.Run();
